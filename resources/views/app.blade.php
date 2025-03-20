2f6594b0ad3092314173124f24fb2cb3db9cdd11
zaimazhar <zaim.azhar97@gmail.com>
2025-03-20 15:27:27 +0800

    Feat: Add Favicon 👾

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,7 @@
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
+        <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
